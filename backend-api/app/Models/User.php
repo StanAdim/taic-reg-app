26d744a2d8a5ff2dd700cd997b6d9f0ddf67854d
StanAdim <stanjustine@gmail.com>
2024-05-28 16:44:59 +0300

    Implement: Fix:Email Verification  mode 4

--- backend-api/app/Models/User.php
+++ backend-api/app/Models/User.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Event\Subscription;
 use App\Models\Taic\Conference;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+// class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable 
 {
     use HasFactory, Notifiable;
 
--- backend-api/resources/views/mail/verification/verifyUserEmail.blade.php
+++ backend-api/resources/views/mail/verification/verifyUserEmail.blade.php
@@ -1,1 +1,31 @@
-hello form email verification link 
+
+<div style="padding:1.4rem;background-color: #e8e8ef; font-size:large; font-family: Verdana, Geneva, Tahoma, sans-serif">
+    <p style="padding:1px 2px; margin: 4px 1px;">Dear<b style="color:#105f8d;"> 
+        {{-- {{$participant->fullName}}, --}}
+        Name placeholder
+    </b>
+    </p>
+<p style="padding:4px 2px; margin: 4px 1px;">
+    {{-- Email message --}}
+We hope this email finds you well. The ICT Commission is delighted to inform you that your 
+registration for the upcoming events has been successfully received. Your participation is greatly appreciated, 
+and we are thrilled to have you join us for these important events.
+
+Please click the link below to verify your email and complete your registration:  
+[Verification Link]
+
+</p>
+
+<p style="padding:4px 2px; margin: 4px 1px;">
+    Should you require any assistance please do not hesitate to contact us. We look forward to welcoming you 
+    to our engaging and insightful event.
+</p>
+<p style="padding:1px 2px; margin: 4px 1px;">Hotline : <span style="color: #105f8d;font-weight:600;">+255 738 171 742</span></p>
+<p style="padding:1px 2px; margin: 4px 1px;">Email : 
+    <span style="color: #105f8d;font-weight:600;">info@ictc.go.tz</span> or 
+    <span style="color: #105f8d;font-weight:600">ictsupport@ictc.go.tz</span>
+</p>
+<p style="padding:1px 2px; margin: 4px 1px;">Thank you!</p>
+<p style="padding:1px 2px; margin: 4px 1px;">--------------</p>
+<p style="font-weight: 800;padding:1px 2px; margin: 4px 1px; font-size: larger">ICT Commission</p>
+</div>
