8fdfd5aa2cfe46ce4ddc11e92956fcd46bab0531
StanAdim <stanjustine@gmail.com>
2024-07-29 09:10:05 +0300

    Production: Rose: Improving bill generation logic

--- frontend-app/stores/useDocumentMaterialStore.ts
+++ frontend-app/stores/useDocumentMaterialStore.ts
@@ -2,6 +2,7 @@ import type {ApiResponse, DocumentMaterial} from "~/types/interfaces";
 
 export const useDocumentMaterialStore = defineStore('documentStore', () => {
 
+    const globalStore = useGlobalDataStore()
     //Stores
     const allDocument= ref <DocumentMaterial[]>([]);
     const eventDocuments= ref <DocumentMaterial[]>([]);
@@ -12,8 +13,23 @@ export const useDocumentMaterialStore = defineStore('documentStore', () => {
 
     //Actions
     const uploadNewDocument = async (passedData)=> {
-        console.log(passedData)
-    }
+        // Make the API request to upload the file
+        try {
+            const { data, error } = await useApiFetch('/api/upload-document', {
+                method: 'POST',
+                body: passedData,
+            });
+            if (error.value) {
+                globalStore.assignAlertMessage(error.value?.message, 'error')
+                throw error.value;
+            }
+            const message = 'Document uploaded successfully!';
+            globalStore.assignAlertMessage(message, 'success');
+        } catch (err) {
+            console.error(err);
+            const message = 'Failed to upload document.';
+            globalStore.assignAlertMessage(message, 'error');
+        }    }
     const handleEventDocument = async ()=> {
         console.log(passedUserInfo)
     }
