dfcb5ded3432cc8e25382ae704a0f73e2f90fcc7
StanAdim <stanjustine@gmail.com>
2024-09-20 14:41:08 +0300

    Fix: handle dublicated 2

--- backend-api/app/Imports/ProfessionalImport.php
+++ backend-api/app/Imports/ProfessionalImport.php
@@ -5,23 +5,36 @@ namespace App\Imports;
 use App\Models\Professional;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\QueryException;
 
 
 class ProfessionalImport implements ToModel, WithHeadingRow
 {
-    public function model(array $row)
-    {
-        return new Professional([
-            'DateOfRegistration'     => $row['dateofregistration'],
-            'RegNo'                  => $row['regno'],
-            'Name'                   => $row['name'],
-            'Employer'               => $row['employer'],
-            'ProfessionalCategory'    => $row['professionalcategory'],
-            'AreaOfSpecialization'    => $row['areaofspecialization'],
-            'Email'                  => $row['email'],
-            'Mobile'                 => $row['mobile'],
-            'Gender'                 => $row['gender'],
-            'Region'                 => $row['region'],
-        ]);
+
+    public function model(array $row){
+        try {
+            return new Professional([
+                'DateOfRegistration'     => $row['dateofregistration'],
+                'RegNo'                  => $row['regno'],
+                'Name'                   => $row['name'],
+                'Employer'               => $row['employer'],
+                'ProfessionalCategory'    => $row['professionalcategory'],
+                'AreaOfSpecialization'    => $row['areaofspecialization'],
+                'Email'                  => $row['email'],
+                'Mobile'                 => $row['mobile'],
+                'Gender'                 => $row['gender'],
+                'Region'                 => $row['region'],
+            ]);   
+         } catch (QueryException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                // Log the duplicate RegNo for further review
+                Log::warning('Duplicate RegNo found: ' . $row['regno']);
+                return null;
+            }
+
+            throw $e;
+        }
     }
+
 }
